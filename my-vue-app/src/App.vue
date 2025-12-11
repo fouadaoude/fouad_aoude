f38550d6e2563d5d22f9f78ef9182ef76113534a
Fouad Aoude <fouad@Fouads-MacBook-Pro.local>
2025-12-11 20:53:59 +0000

    added Vue.js and Flask

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1>Vue Program for CIS 285 By Fouad Aoude</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
